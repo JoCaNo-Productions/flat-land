d6e44ddcce3948276365ff254f97320210e58e8a
Noah <noahmouse2011@gmail.com>
2018-07-29 16:16:39 -0600

    Added constants.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+# Return values for Champion abilities
+ABILITY_NOT_SET = -2 # Default return value in base class
+PASSIVE = -1 # For passive abilities
+
+SUCCESS = 0
+INSUFF_MANA = 1 # Insufficient mana
+ABILITY_NOT_COOLED = 2
+OUT_OF_RANGE = 3
