34473c38b738884e5c76e1b35626c5022cfcbefb
calebmrose00 <41278785+calebmrose00@users.noreply.github.com>
2018-07-30 12:21:29 -0600

    Update gillan.py

--- champs/gillan.py
+++ champs/gillan.py
@@ -1,4 +1,4 @@
-# Status: Being Worked On (Caleb)
+# Status: Needs Review and discussion (Caleb)
 
 from base_classes import Champion
 from mods import Mod, dot
@@ -6,9 +6,18 @@ from mods import Mod, dot
 class Gillan(Champion):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
+		self.player.inventory.append(Shield_Cons())
+		self.abl3_active=False
+		self.self.shield_feature=None
 	
-	def move(self, loc):
-		super().move(loc)
+	def damage(self, amount:int, defend: bool=True, magic_attack: bool=False):
+		if abl3_active==True:
+			if defend==False:
+				return FAILED_ATTACK
+			else:
+				return super().damage(amount= amount, defend=defend, magic_attack=magic_attack)
+		else:
+			return super().damage(amount= amount, defend=defend, magic_attack=magic_attack)
 
 	def ability1(self) -> None:
 		'''Gillan.ability1() -> None
@@ -18,73 +27,74 @@ class Gillan(Champion):
 			self.game.alert('Not Enough Mana')
 			return 1
 		self.mana -= 5
-		
+		self.mods['speed'].append(charge_mod(self))
 		return 0
 
 	def ability2(self):
 		'''Gillan.ability2() -> None
 		
-		Name - 0m. Desc.'''
-		target = self.game.selectplayer()
-		if self.mana < 0:
-			self.game.alert('Not Enough Mana')
-			return 1
-		if self.cooldowns[0] > 0:
-			self.game.alert(f'{self.ability_names[0]} ability has not cooled down yet')
-			return 2
-		if False: # Check for range
-			self.game.alert("out of range")
-			return 3 # Out of range return value
-		if False:
-			self.game.alert(f'Reached Cap of ___ on player {target.player.nick}')
-			return 4
-		self.mana -= 0
-		self.cooldown[0] = 0
+		Drop Shield: Leaves a "Shield" Terrain Were Gillan Was standing.
+		Gillan Can not Use Shield of Strength until retrieving his shield.
+		gain a bonus 1.2 movement speed but loses all Defense. 0m'''
+		
+		shield=False
+		for x in self.player.inventory:
+			if x.nick=='Shield_Cons':
+				shield=True
+		
+		if shield ==False:
+			self.game.alert('No shield')
+			return 17
+		
+		self.shield_feature=Shield_Feat(self.pos)
+		# ISSUE: We need to work out how items work
 		return 0
 		
 
 	def ability3(self):
 		'''Gillan.ability3() -> None
 		
-		Name - 0m. Desc.'''
-		target = self.game.selectplayer()
-		if self.mana < 0:
+		Shield of Strength: Ignore all True Damage Gained this turn. five turn cool down. 8m'''
+		if self.mana < 8:
 			self.game.alert('Not Enough Mana')
 			return 1
-		if self.cooldowns[0] > 0:
-			self.game.alert(f'{self.ability_names[0]} ability has not cooled down yet')
+		if self.cooldowns[0] > 10:
+			self.game.alert(f'{self.ability_names[2]} ability has not cooled down yet')
 			return 2
-		if False: # Check for range
-			self.game.alert("out of range")
-			return 3 # Out of range return value
-		if False:
-			self.game.alert(f'Reached Cap of ___ on player {target.player.nick}')
-			return 4
-		self.mana -= 0
+		self.mana -= 8
 		self.cooldown[0] = 0
+		self.abl3_active=True
+		self.mods['health'].append(shield_of_strength_mod)
 		return 0
 
 	def ult(self):
 		'''Gillan.ult() -> None
 		
-		Name - 0m. Desc.'''
-		target = self.game.selectplayer()
-		if self.mana < 0:
+		Call of the Shield: Gillan can after Channeling
+		(not Doing anything) for one turn, teleport back to
+		his shield if it was dropped. 20m'''
+		if self.mana < 20:
 			self.game.alert('Not Enough Mana')
 			return 1
-		if self.cooldowns[0] > 0:
-			self.game.alert(f'{self.ability_names[0]} ability has not cooled down yet')
-			return 2
-		if False: # Check for range
-			self.game.alert("out of range")
-			return 3 # Out of range return value
-		if False:
-			self.game.alert(f'Reached Cap of ___ on player {target.player.nick}')
-			return 4
-		self.mana -= 0
-		self.cooldown[0] = 0
+		if self.shield_feature!=None
+		self.mana -= 20
+		self.channel(length=1,action=self.ability3_afterchanneling)
 		return 0
+	
+	def ult_afterchanneling(self) -> None:
+		'''Gillan._afterchanneling() -> None'''
+		self.move(self.self.shield_feature.pos)
 
+class Shield_Cons(Consumable):
+	def __init__(self):
+		self.defense = 10
+		self.speed=-1.2
+		self.nick='Shield_Cons'
+
+class Shield_Feat(Feature):
+	def __init__(self,pos):
+		self.pos=pos
+	
 d = {
 	'health':0,
 	'max_health':0,
@@ -102,7 +112,7 @@ d = {
 }
 
 def charge_mod(champ): # Return Mod that handles ability
-	assert isinstance(champ, DarkAtKnight), 'Attempt to add charge_mod Mod() to non-Gillan champ'
+	assert isinstance(champ, Gillan), 'Attempt to add charge_mod Mod() to non-Gillan champ'
 	start_speed = .5
 	decrement=.1
 	if start_damage%decrement != 0:
@@ -119,7 +129,18 @@ def charge_mod(champ): # Return Mod that handles ability
 			  )
 	return mod
 
+def shield_of_strength_mod(champ):
+	assert isinstance(champ, Gillan), 'Attempt to add shield_of_strength_mod Mod() to non-Gillan champ'
+	def next_turn():
+		champ.abl3_active=False
+	mod = Mod(priority=5,
+			  turn=next_turn,
+			  lifetime=1,
+			  )
+	return mod
 
+	
+	
 def create(): # Return instance of classname, used by champion select
-	return classname(**d)
+	return Gillan(**d)
 
