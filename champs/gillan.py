330ec68f3b0c61339f5a0340eca690cee8c73207
calebmrose00 <41278785+calebmrose00@users.noreply.github.com>
2018-07-30 09:27:49 -0600

    Update gillan.py

--- champs/gillan.py
+++ champs/gillan.py
@@ -1,9 +1,9 @@
-# Status: Defaults
+# Status: Being Worked On (Caleb)
 
 from base_classes import Champion
 from mods import Mod, dot
 
-class classname(Champion):
+class Gillan(Champion):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 	
@@ -11,28 +11,18 @@ class classname(Champion):
 		super().move(loc)
 
 	def ability1(self) -> None:
-		'''classname.ability1() -> None
+		'''Gillan.ability1() -> None
 		
-		Name - 0m. Desc.'''
-		target = self.game.selectplayer()
-		if self.mana < 0:
+		Charge: Gain a Bonus .5 decaying Movement Speed. 5m'''
+		if self.mana < 5:
 			self.game.alert('Not Enough Mana')
 			return 1
-		if self.cooldowns[0] > 0:
-			self.game.alert(f'{self.ability_names[0]} ability has not cooled down yet')
-			return 2
-		if False: # Check for range
-			self.game.alert("out of range")
-			return 3 # Out of range return value
-		if False:
-			self.game.alert(f'Reached Cap of ___ on player {target.player.nick}')
-			return 4
-		self.mana -= 0
-		self.cooldown[0] = 0
+		self.mana -= 5
+		
 		return 0
 
 	def ability2(self):
-		'''classname.ability2() -> None
+		'''Gillan.ability2() -> None
 		
 		Name - 0m. Desc.'''
 		target = self.game.selectplayer()
@@ -54,7 +44,7 @@ class classname(Champion):
 		
 
 	def ability3(self):
-		'''classname.ability3() -> None
+		'''Gillan.ability3() -> None
 		
 		Name - 0m. Desc.'''
 		target = self.game.selectplayer()
@@ -75,7 +65,7 @@ class classname(Champion):
 		return 0
 
 	def ult(self):
-		'''classname.ult() -> None
+		'''Gillan.ult() -> None
 		
 		Name - 0m. Desc.'''
 		target = self.game.selectplayer()
@@ -111,8 +101,24 @@ d = {
 	'magic_resist':0,
 }
 
-def mod(var): # Return Mod that handles ability
-	pass
+def charge_mod(champ): # Return Mod that handles ability
+	assert isinstance(champ, DarkAtKnight), 'Attempt to add charge_mod Mod() to non-Gillan champ'
+	start_speed = .5
+	decrement=.1
+	if start_damage%decrement != 0:
+		raise Exception
+	d = {'speed':start_damage}
+	def modify(speed):
+		return speed+d['speed']
+	def next_turn():
+		d['speed'] -= decrement
+	mod = Mod(priority=5,
+			  mod=modify,
+			  turn=next_turn,
+			  lifetime=start_speed//decrement,
+			  )
+	return mod
+
 
 def create(): # Return instance of classname, used by champion select
 	return classname(**d)
